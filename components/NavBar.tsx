2f1fde55b5b17bffdf9f36e754a35dfbd657cf86
Toshiki <toshiki.fujii@outlook.com>
2023-12-11 08:04:12 +0900

    create cart components

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,6 +3,7 @@ import MaxWidthWrapper from "./MaxWidthWrapper";
 import { Icons } from "./Icons";
 import NavItems from "./ui/NavItems";
 import { buttonVariants } from "./ui/button";
+import Cart from "./Cart";
 
 const NavBar = () => {
   const user = null;
@@ -62,7 +63,9 @@ const NavBar = () => {
                     </div>
                   )}
 
-                  <div className="ml-4 flow-root lg:ml-6">{/* <Cart /> */}</div>
+                  <div className="ml-4 flow-root lg:ml-6">
+                    <Cart />
+                  </div>
                 </div>
               </div>
             </div>
