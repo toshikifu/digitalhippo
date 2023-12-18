a9ac55068beffb4594ecd6263ec30a21da487e94
Toshiki <toshiki.fujii@outlook.com>
2023-12-18 17:48:54 +0900

    verification

--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -2,6 +2,7 @@ import { TRPCError } from "@trpc/server";
 import { getPayloadClient } from "../get-payload";
 import { AuthCredentialsValidator } from "../lib/valitators/accont-credentialsvalidator";
 import { publicProcedure, router } from "./trpc";
+import { z } from "zod";
 
 export const authRouter = router({
   createPayloadUser: publicProcedure
@@ -31,5 +32,22 @@ export const authRouter = router({
         },
       });
       return { success: true, sentEmail: email };
+    }),
+
+  verifyEmail: publicProcedure
+    .input(z.object({ token: z.string() }))
+    .query(async ({ input }) => {
+      const { token } = input;
+
+      const payload = await getPayloadClient();
+
+      const isVerified = await payload.verifyEmail({
+        collection: "users",
+        token,
+      });
+
+      if (!isVerified) throw new TRPCError({ code: "UNAUTHORIZED" });
+
+      return { success: true };
     }),
 });
